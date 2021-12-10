024652f5e592270e6cb93f1626d221a775a96dc5
Soham20chak <81106480+Soham20chak@users.noreply.github.com>
2021-12-11 03:39:23 +0530

    Delete SWE directory

--- SWE/api.js
+++
@@ -1,6 +0,0 @@
-let API_KEY = 'a22660cc1d884fd967730b95a4aea6cf';
-let IMG_URL='https://image.tmdb.org/t/p/w500';
-let movie_detail_http="https://api.themoviedb.org/3/movie";
-let original_img_url='https://image.tmdb.org/t/p/original';
-let movie_genres_http="https://api.themoviedb.org/3/discover/movie?";
-let genres_list_http="https://api.themoviedb.org/3/genre/movie/list?";
